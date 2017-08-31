038c5ef8c1a81f945f485946a8336db4f1fb884b
Samuel Gaamuwa <samuel.gaamuwa@spatialkey.com>
2017-08-31 04:15:02 +0300

    adds styling elements for the application

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,18 +8,31 @@ public class Word {
 
     private String englishTranslation;
     private String miwokTranslation;
+    private int imageResource = NO_IMAGE_RESOURCE;
+    private static final int NO_IMAGE_RESOURCE = -1;
 
     public Word(String englishTranslation, String miwokTranslation){
         this.englishTranslation = englishTranslation;
         this.miwokTranslation = miwokTranslation;
     }
 
+    public Word(String englishTranslation, String miwokTranslation, int imageResource){
+        this(englishTranslation, miwokTranslation);
+        this.imageResource = imageResource;
+    }
+
     public String getDefaultTranslation(){
-        return englishTranslation;
+        return this.englishTranslation;
     }
 
     public String getMiwokTranslation(){
-        return miwokTranslation;
+        return this.miwokTranslation;
+    }
+
+    public int getImageResource() { return this.imageResource; }
+
+    public boolean hasImage(){
+        return imageResource != NO_IMAGE_RESOURCE;
     }
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,10 +1,13 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -15,8 +18,11 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Context context, ArrayList<Word> words){
+    private int categoryColour;
+
+    public WordAdapter(Context context, ArrayList<Word> words, int categoryColour){
         super(context, 0, words);
+        this.categoryColour = categoryColour;
     }
 
     @Override
@@ -32,6 +38,7 @@ public class WordAdapter extends ArrayAdapter<Word> {
         if (word != null) {
             TextView tt1 = (TextView) convertView.findViewById(R.id.miwak_number);
             TextView tt2 = (TextView) convertView.findViewById(R.id.english_number);
+            ImageView vv1 = (ImageView) convertView.findViewById(R.id.image_miwak);
 
             if (tt1 != null) {
                 tt1.setText(word.getMiwokTranslation());
@@ -40,7 +47,20 @@ public class WordAdapter extends ArrayAdapter<Word> {
             if (tt2 != null) {
                 tt2.setText(word.getDefaultTranslation());
             }
+            if(vv1 != null) {
+                if(word.hasImage()){
+                    vv1.setImageResource(word.getImageResource());
+                } else {
+                    vv1.setVisibility(View.GONE);
+                }
+            }
         }
+
+        View textContainer = convertView.findViewById(R.id.textCard);
+
+        int color = ContextCompat.getColor(getContext(), categoryColour);
+
+        textContainer.setBackgroundColor(color);
 
         return convertView;
     }
