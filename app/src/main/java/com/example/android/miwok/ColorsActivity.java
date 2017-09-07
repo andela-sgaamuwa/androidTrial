137ef6151b0db304e2ac053ba5ed99d294d287df
Samuel Gaamuwa <samuel.gaamuwa@spatialkey.com>
2017-09-07 04:53:25 +0300

    Modify all Word objects to take a sound resource Id

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -29,14 +29,14 @@ public class ColorsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_colors);
 
         ArrayList<Word> words = new ArrayList<>();
-        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red));
-        words.add(new Word("green", "chokokki", R.drawable.color_green));
-        words.add(new Word("brown", "takaakki", R.drawable.color_brown));
-        words.add(new Word("gray", "topoppi", R.drawable.color_gray));
-        words.add(new Word("black", "kululli", R.drawable.color_black));
-        words.add(new Word("white", "kelelli", R.drawable.color_white));
-        words.add(new Word("dusty yellow", "topiisa", R.drawable.color_dusty_yellow));
-        words.add(new Word("mustard yellow", "chiwiite", R.drawable.color_mustard_yellow));
+        words.add(new Word("red", "weṭeṭṭi",R.raw.color_red, R.drawable.color_red));
+        words.add(new Word("green", "chokokki", R.raw.color_green ,R.drawable.color_green));
+        words.add(new Word("brown", "takaakki", R.raw.color_brown, R.drawable.color_brown));
+        words.add(new Word("gray", "topoppi", R.raw.color_gray, R.drawable.color_gray));
+        words.add(new Word("black", "kululli", R.raw.color_black, R.drawable.color_black));
+        words.add(new Word("white", "kelelli", R.raw.color_white, R.drawable.color_white));
+        words.add(new Word("dusty yellow", "topiisa", R.raw.color_dusty_yellow, R.drawable.color_dusty_yellow));
+        words.add(new Word("mustard yellow", "chiwiite", R.raw.color_mustard_yellow, R.drawable.color_mustard_yellow));
 
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
 
